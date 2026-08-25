2437dc8d96936ea239f2494d5f8ab0d00bb18030
Leela Prasad Guvvala <leelaprasad.guvvala@gmail.com>
2026-08-25 06:02:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 56.8 MB (32.35%)

---
+++ submissions/4080-smallest-missing-multiple-of-k/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var missingMultiple = function(nums, k) {
+
+    const numSet = new Set(nums);
+    let multiple = k;
+    
+    while (numSet.has(multiple)) {
+        multiple += k;
+    }
+    
+    return multiple;
+    
+};
